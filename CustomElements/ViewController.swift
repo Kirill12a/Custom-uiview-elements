eaba9d73cb270fced4fb15d62420d0db8226ab17
Kirill <kirilldrozd12@gmail.com>
2022-01-28 17:06:12 +0300

    Initial Commit

---
+++ CustomElements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomElements
+//
+//  Created by Kirill Drozdov on 28.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
